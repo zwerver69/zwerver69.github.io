11af80d3454138b5d6ba5cc3b3ed385416cabe80
zwerver69 <126071892+zwerver69@users.noreply.github.com>
2023-07-30 21:29:50 +0200

    update 0.1

--- .web/pages/index.js
+++ .web/pages/index.js
@@ -2,7 +2,7 @@ import {useEffect, useRef, useState} from "react"
 import {useRouter} from "next/router"
 import {E, connect, updateState} from "/utils/state"
 import "focus-visible/dist/focus-visible"
-import {Center, Heading, VStack, useColorMode} from "@chakra-ui/react"
+import {Box, Center, HStack, Heading, Text, useColorMode} from "@chakra-ui/react"
 import NextHead from "next/head"
 
 const EVENT = "ws://localhost:8000/event"
@@ -41,11 +41,21 @@ useEffect(() => {
   update()
 })
 return (
-<Center sx={{"paddingTop": "10%", "backgroundImage": "linear-gradient(0deg, rgb(0,0,0), rgb(0,0,100))", "height": "100vh"}}><VStack spacing="1.5em"
-sx={{"fontSize": "2em"}}><Heading sx={{"fontSize": "2em", "color": "white"}}>{`hello world`}</Heading></VStack>
-<NextHead><title>{`Pynecone App`}</title>
+<Center sx={{"paddingTop": "10%", "backgroundColor": "rgb(128,128,128)", "height": "100vh"}}><HStack spacing="1.5em"
+sx={{"fontSize": "2em"}}><Heading sx={{"fontSize": "2em", "color": "black", "width": "90%"}}>{`Arthur_`}</Heading>
+<Box sx={{"padding": "2%", "borderRadius": 10, "width": "140%", "boxShadow": "-2px 2px 10px 0px rgba(0, 0, 0, 0.1)", "border": "2px solid rgba(200, 200, 200)", "backgroundColor": "rgb(240, 240, 240)"}}><Heading>{`About`}</Heading>
+<Text>{`
+-Hello, I'm Arthur.
+                    `}</Text>
+<Text>{` -My birthday is the 10th January.`}</Text>
+<HStack><Text>{`- It's: `}</Text>
+<Box dangerouslySetInnerHTML={{"__html": "\n<iframe src=\"https://free.timeanddate.com/clock/i8yjdned/n48/tlbe/tct/pct/ftb/bo2/tt0/tw1/tm1/tb4\" frameborder=\"0\" width=\"123\" height=\"39\" allowtransparency=\"true\"></iframe>\n                        "}}/></HStack>
+<Text>{`- I speak Dutch, English and German.`}</Text></Box>
+<Box sx={{"padding": "2%", "borderRadius": 10, "width": "140%", "boxShadow": "-2px 2px 10px 0px rgba(0, 0, 0, 0.1)", "border": "2px solid rgba(200, 200, 200)", "backgroundColor": "rgb(240, 240, 240)"}}><Heading>{`Projects`}</Heading>
+<Text>{`- Tess (Discord bot)`}</Text></Box></HStack>
+<NextHead><title>{`home-arthur_`}</title>
 <meta name="description"
-content="A Pynecone app."/>
+content="The website that's tell you a little more about arthur_"/>
 <meta property="og:image"
 content="favicon.ico"/></NextHead></Center>
 )
--- zwerver_com/zwerver_com.py
+++ zwerver_com/zwerver_com.py
@@ -20,19 +20,49 @@ class State(pc.State):
 
 def index() -> pc.Component:
     return pc.center(
-        pc.vstack(
-            pc.heading("hello world!", font_size="2em", color="white"),
-                    
-
+        pc.hstack(
+            pc.heading("Arthur_", font_size="2em", color="black", width="90%"),
+            pc.box(
+        pc.heading("About"),
+                pc.text(
+                    """
+-Hello, I'm Arthur.
+                    """
+                ),
+                pc.text(" -My birthday is the 10th January."),
+                pc.hstack(
+                    pc.text("- It's: "),
+                    pc.html("""
+<iframe src="https://free.timeanddate.com/clock/i8yjdned/n48/tlbe/tct/pct/ftb/bo2/tt0/tw1/tm1/tb4" frameborder="0" width="123" height="39" allowtransparency="true"></iframe>
+                        """),
+                ),
+                pc.text("- I speak Dutch, English and German."),
+                padding="2%",
+                border_radius=10,
+                width="140%",
+                box_shadow="-2px 2px 10px 0px rgba(0, 0, 0, 0.1)",
+                border="2px solid rgba(200, 200, 200)",
+                background_color="rgb(240, 240, 240)"
+            ),
+            pc.box(
+                pc.heading("Projects"),
+                pc.text("- Tess (Discord bot)"),
+                padding="2%",
+                border_radius=10,
+                width="140%",
+                box_shadow="-2px 2px 10px 0px rgba(0, 0, 0, 0.1)",
+                border="2px solid rgba(200, 200, 200)",
+                background_color="rgb(240, 240, 240)"
+            ),
             spacing="1.5em",
             font_size="2em",
         ),
         padding_top="10%",
-        background_image= "linear-gradient(0deg, rgb(0,0,0), rgb(0,0,100))", height="100vh"
+        background_color= "rgb(128,128,128)", height="100vh"
     )
 
 
 # Add state and page to the app.
 app = pc.App(state=State,style=styles)
-app.add_page(index)
+app.add_page(index, title="home-arthur_", description="The website that's tell you a little more about arthur_")
 app.compile()
